a6585c4f6fe8d2b90ae61872fcc7a7e6905e08a6
Martin Blomberg <martin.blomberg@outlook.com>
2022-01-14 13:48:36 +0100

    Add BracketsTest.java

---
+++ src/test/java/org/example/BracketsTest.java
@@ -0,0 +1,90 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BracketsTest {
+
+    @Test
+    void emptyStringShouldBeValid() {
+        Brackets brackets = new Brackets();
+
+        boolean result = brackets.isValid("");
+
+        assertTrue(result);
+    }
+
+    @Test
+    void unevenNumberOfBracketsShouldNotBeValid() {
+        Brackets brackets = new Brackets();
+
+        boolean result = brackets.isValid("(()");
+
+        assertFalse(result);
+    }
+
+    @Test
+    void startingWithClosingBracketShouldBeInvalid() {
+        Brackets brackets = new Brackets();
+
+        boolean result = brackets.isValid(")(");
+
+        assertFalse(result);
+    }
+
+    @Test
+    void incorrectlyPositionedBracketsLaterInSequenceShouldBeInvalid() {
+        Brackets brackets = new Brackets();
+
+        boolean result = brackets.isValid("())(");
+
+        assertFalse(result);
+    }
+
+    @Test
+    void twoBracketTypesShouldBeValid() {
+        Brackets brackets = new Brackets();
+
+        boolean result = brackets.isValid("([])");
+
+        assertTrue(result);
+    }
+
+    @Test
+    void incorrectStartAndCloseBracketsShouldFail() {
+        Brackets brackets = new Brackets();
+
+        boolean result = brackets.isValid("([))");
+
+        assertFalse(result);
+    }
+
+    @Test
+    void onlyOpeningBracketsShouldFail() {
+        Brackets brackets = new Brackets();
+
+        boolean result = brackets.isValid("((((");
+
+        assertFalse(result);
+    }
+
+    @Test
+    void wrongOrderOfMultipleBracketsShouldFail() {
+        Brackets brackets = new Brackets();
+
+        boolean result = brackets.isValid("([)]");
+
+        assertFalse(result);
+    }
+
+    @Test
+    void complicatedBracketsShouldBeValid() {
+        Brackets brackets = new Brackets();
+
+        boolean result = brackets.isValid("([{}([()])])");
+
+        assertTrue(result);
+    }
+}
