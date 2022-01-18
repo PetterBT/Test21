bf1daf729b435fbb32ed11c8b671e0103c501f49
PetterBT <petter.bergstrom@iths.se>
2022-01-18 20:52:36 +0100

    Initial commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,9 @@
 package org.example;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello There!");
+    public static void main(String[] args) {System.out.println("Hello There!");}
+
+    public int sum(int a, int b) {
+        return a + b;
     }
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,12 +1,25 @@
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AppTest {
     @Test
     void test() {
         assertThat(true).isTrue();
+    }
+
+    @Test
+    @DisplayName("Sum should return three for input one and two")
+    void sumOfOneAndTwoReturnsThree() {
+        App app = new App();
+
+        var result = app.sum(1,2);
+
+        assertEquals(3, result, "1 + 2 should return 3");
+
     }
 }
