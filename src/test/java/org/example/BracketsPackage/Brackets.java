046eb2da9f086236a9c338c954d90c0f94671315
PetterBT <petter.bergstrom@iths.se>
2022-01-21 13:49:31 +0100

    Add Brackets.java and isValid-method in BracketsTest.java

---
+++ src/test/java/org/example/BracketsPackage/Brackets.java
@@ -0,0 +1,9 @@
+package org.example.BracketsPackage;
+
+public class Brackets {
+
+    public boolean isValid(String s) {
+        return false;
+
+    }
+}
--- src/test/java/org/example/BracketsPackage/BracketsTest.java
+++ src/test/java/org/example/BracketsPackage/BracketsTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.BracketsPackage;
 
 import org.junit.jupiter.api.Test;
 
