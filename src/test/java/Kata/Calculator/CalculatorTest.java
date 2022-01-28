2a4523bafa43628fe70e47548d9ad6068b44f9c2
PetterBT <petter.bergstrom@iths.se>
2022-01-28 15:03:53 +0100

    Modify code to add delimiter change and add test

--- src/test/java/Kata/Calculator/CalculatorTest.java
+++ src/test/java/Kata/Calculator/CalculatorTest.java
@@ -25,12 +25,12 @@ public class CalculatorTest {
         assertThat(Calculator.add("1,2,3,50,944")).isEqualTo(1000);
     }
     @Test
-    void should_accept_new_line_instead_of_comma() {
-        assertThat(Calculator.add("2\n6,2")).isEqualTo(10);
+    void should_accept_new_line_instead_of_comma() {                    // Needs work
+        assertThat(Calculator.add("2,\n")).isEqualTo(2);
     }
     @Test
-    void should_ret() {
-        assertThat(Calculator.add("")).isEqualTo(0);
+    void should_change_delimiter_when_string_begins_with_two_forwardslash() {
+        assertThat(Calculator.add("//;\n2;4;4")).isEqualTo(10);
     }
 
 }
--- src/test/java/org/example/Calculator.java
+++ src/test/java/org/example/Calculator.java
@@ -1,20 +1,39 @@
 package org.example;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Calculator {
     public static int add(String numbers) {
         if (!numbers.isEmpty()) {
-            List<Integer> numbersList =Arrays.stream(numbers.split("[,\n]"))
-                    .map(Integer::parseInt)
-                    .collect(Collectors.toList());
+            List<Integer> numbersList = stringArrayToIntList(getSplit(numbers));
 
             return numbersList.stream()
                     .reduce(Integer::sum)
                     .orElseThrow();
         }
         return 0;
+    }
+
+    private static List<Integer> stringArrayToIntList(String[] numbers) {
+        return Arrays.stream(numbers)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    private static String[] getSplit(String numbers) {
+        if (numbers.startsWith("//")) {
+            Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(numbers);
+            if (matcher.matches()) {
+                String delimiter = matcher.group(1);
+                String toSplit = matcher.group(2);
+                return toSplit.split(delimiter);
+            }
+        }
+        return numbers.split(",\n");
     }
 }
